a14f3dca58b37888fa6728ef49a6f03ee6a48762
Karambir Singh Nain <akarambir@gmail.com>
2013-02-11 03:58:08 +0530

    settings with user and student models

--- cdi/forms.py
+++ cdi/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from student.models import Student
 
-class SettingsForm(ModelForm):
+class UserForm(ModelForm):
     class Meta:
         model=User
         fields = ('first_name', 'last_name')
+
+class StudentForm(ModelForm):
+    class Meta:
+        model=Student
+        fields = ('college', 'course', 'batch')
--- cdi/views.py
+++ cdi/views.py
@@ -3,21 +3,27 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from cdi.forms import SettingsForm
+from cdi.forms import UserForm, StudentForm
 
 from django.contrib.auth.models import User
+from student.models import Student
 
 @login_required
 def settings(request):
-    form = None
+    form1 = None
+    form2 = None
     u = User.objects.get(email=request.user.email)
+    s = Student.objects.get(user=u)
     if request.method == 'POST':
-        form = SettingsForm(request.POST, instance=u)
-        if form.is_valid():
+        form1 = UserForm(request.POST, instance=u)
+        form2 = StudentForm(request.POST, instance=s)
+        if all([form1.is_valid(), form2.is_valid()]):
             messages.add_message(request, messages.SUCCESS, ('Your settings have been saved'))
-            form.save()
+            form1.save()
+            form2.save()
             return HttpResponseRedirect('/accounts/profile/')
     else:
-        form = SettingsForm(instance=u)
-    return render_to_response('settings.html', {'form': form}, context_instance=RequestContext(request))
+        form1 = UserForm(instance=u)
+        form2 = StudentForm(instance=s)
+    return render_to_response('settings.html', {'form1': form1, 'form2': form2}, context_instance=RequestContext(request))
 
