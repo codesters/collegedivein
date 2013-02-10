a36f36acbcdacef71e1785a36243e83d1dbda2bf
Karambir Singh Nain <akarambir@gmail.com>
2013-02-10 20:14:14 +0530

    signup workin both ways and settings pages done

--- cdi/urls.py
+++ cdi/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     (r'^accounts/', include('allauth.urls')),
     url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html' }),
-    url(r'^accounts/profile/$', 'django.views.generic.simple.direct_to_template', {'template': 'profile.html' }, name="profile"),
+    url(r'^accounts/profile/$', 'django.views.generic.simple.direct_to_template', {'template': 'profile.html' }, name="account_profile"),
     url(r'^accounts/settings/$', 'cdi.views.settings', name='account_settings'),
     url(r'^admin/', include(admin.site.urls)),
 )
